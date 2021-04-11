7ade1bd92fe0f12be5c9d95a55ec0495c99d9e43
Christoph Peiniger <christoph.peiniger@googlemail.com>
2021-04-11 21:00:29 +0300

    add get element by if for dates and gender

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 function information(){
-  var male=document
+  var male=document.getElementById("maleInput").checked;
+  var female=document.getElementById("femaleInput").checked;
+  var day=document.getElementById("dayInput").value;
+  var month=document.getElementById("monthInput").value;
+  var year=document.getElementById("yearInput").value;
 
-
-
+  var 
 
 }
